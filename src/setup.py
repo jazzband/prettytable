6b335659cab8d4980496cf37eb96872c3d75a5c4
luke.maurits <luke.maurits@0f58610c-415a-11de-9c03-5d6cfad8e937>
2009-05-26 07:57:24 +0000

    Initial import to Google code repository, take 2!

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='prettytable',
+    version='TRUNK',
+    description='A simple Python library for easily displaying tabular data in a visually appealing ASCII table format',
+    author='Luke Maurits',
+    author_email='luke@maurits.id.au',
+    url='http://www.luke.maurits.id.au/software/prettytable',
+    license='http://www.luke.maurits.id.au/software/bsdlicense.txt',
+    py_modules=['prettytable']
+)
