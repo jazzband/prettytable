9584777803f1d53ecec0860928c185531513d5eb
luke <luke@maurits.id.au>
2012-05-02 02:36:52 +0000

    Copied trunk to 0.6 branch and updated non-source files in preparation for 0.6.

--- src/setup.py
+++ src/setup.py
@@ -5,10 +5,17 @@ from prettytable import __version__ as version
 setup(
     name='prettytable',
     version=version,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Text Processing'
+    ],
     description='A simple Python library for easily displaying tabular data in a visually appealing ASCII table format',
     author='Luke Maurits',
     author_email='luke@maurits.id.au',
-    url='http://www.luke.maurits.id.au/software/prettytable',
-    license='http://www.luke.maurits.id.au/software/bsdlicense.txt',
+    url='http://code.google.com/p/prettytable',
     py_modules=['prettytable']
 )
