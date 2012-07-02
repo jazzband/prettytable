b1abda439f869588217632c37657c8606eac7800
luke <luke@maurits.id.au>
2012-07-02 08:09:43 +0000

    Lots of work to support appropriate alignment/padding/etc. of text using non-Latin characters (e.g. Kanji, Cyrillic, etc.).  Note that this has currently been achieved by using a modified version of code from the "kitchen" library.  Kitchen is LGPL, and since I had to modify their code (to get Python 3 support working), if I were to distribute the current trunk code it would need to be LGPLed instead of BSD licensed.  If I don't want to switch to LGPL for PrettyTable I need to either get permission from everyone who has copyright on the modified code (could be hard) or do a clean room reimplementation.  Fun times ahead!

--- prettytable.py
+++ prettytable.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python
 #
 # Copyright (c) 2009, Luke Maurits <luke@maurits.id.au>
@@ -36,11 +37,19 @@ import csv
 import random
 import sys
 import textwrap
+import itertools
+import unicodedata
 
 py3k = sys.version_info[0] >= 3
 if py3k:
     unicode = str
     basestring = str
+    itermap = map
+    iterzip = zip
+else: 
+    itermap = itertools.imap
+    iterzip = itertools.izip
+
 if py3k and sys.version_info[1] >= 2:
     from html import escape
 else:
@@ -60,7 +69,7 @@ RANDOM = 20
 def _get_size(text):
     lines = text.split("\n")
     height = len(lines)
-    width = max([len(line) for line in lines])
+    width = max([textual_width(line) for line in lines])
     return (width, height)
         
 class PrettyTable(object):
@@ -152,6 +161,27 @@ class PrettyTable(object):
         if not isinstance(value, unicode):
             value = unicode(value, self.encoding, "strict")
         return value
+
+    def _justify(self, text, width, align):
+        excess = width - textual_width(text)
+        if align == "l":
+            return text + excess * " "
+        elif align == "r":
+            return excess * " " + text
+        else:
+            if excess % 2:
+                # Uneven padding
+                # Put more space on right if text is of odd length...
+                if textual_width(text) % 2:
+                    return (excess//2)*" " + text + (excess//2 + 1)*" "
+                # and more space on left if text is of even length
+                else:
+                    return (excess//2 + 1)*" " + text + (excess//2)*" "
+                # Why distribute extra space this way?  To match the behaviour of
+                # the inbuilt str.center() method.
+            else:
+                # Equal padding on either side
+                return (excess//2)*" " + text + (excess//2)*" "
 
     def __getattr__(self, name):
 
@@ -314,7 +344,7 @@ class PrettyTable(object):
 
     def _validate_single_char(self, name, val):
         try:
-            assert len(val) == 1
+            assert textual_width(val) == 1
         except AssertionError:
             raise Exception("Invalid value for %s!  Must be a string of length 1." % name)
 
@@ -922,13 +952,13 @@ class PrettyTable(object):
         return "".join(bits)
 
     def _stringify_row(self, row, options):
-        
+       
         for index, field, value, width, in zip(range(0,len(row)), self._field_names, row, self._widths):
             # Enforce max widths
             lines = value.split("\n")
             new_lines = []
             for line in lines: 
-                if len(line) > width:
+                if textual_width(line) > width:
                     line = textwrap.fill(line, width)
                 new_lines.append(line)
             lines = new_lines
@@ -959,12 +989,7 @@ class PrettyTable(object):
                 if options["fields"] and field not in options["fields"]:
                     continue
 
-                if self._align[field] == "l":
-                    bits[y].append(" " * lpad + l.ljust(width) + " " * rpad)
-                elif self._align[field] == "r":
-                    bits[y].append(" " * lpad + l.rjust(width) + " " * rpad)
-                else:
-                    bits[y].append(" " * lpad + l.center(width) + " " * rpad)
+                bits[y].append(" " * lpad + self._justify(l, width, self._align[field]) + " " * rpad)
                 if options["border"]:
                     bits[y].append(self.vertical_char)
 
@@ -1147,3 +1172,169 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+#
+# Copyright (c) 2010 Red Hat, Inc.
+# Copyright (c) 2010 Ville Skyttä
+# Copyright (c) 2009 Tim Lauridsen
+# Copyright (c) 2007 Marcus Kuhn
+#
+# kitchen is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# kitchen is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with kitchen; if not, see <http://www.gnu.org/licenses/>
+#
+# Authors:
+#   James Antill <james@fedoraproject.org>
+#   Marcus Kuhn
+#   Toshio Kuratomi <toshio@fedoraproject.org>
+#   Tim Lauridsen
+#   Ville Skyttä
+#
+# Portions of this are from yum/i18n.py
+
+
+def _interval_bisearch(value, table):
+    minimum = 0
+    maximum = len(table) - 1
+    if value < table[minimum][0] or value > table[maximum][1]:
+        return False
+
+    while maximum >= minimum:
+        mid = (minimum + maximum) / 2
+        if value > table[mid][1]:
+            minimum = mid + 1
+        elif value < table[mid][0]:
+            maximum = mid - 1
+        else:
+            return True
+
+    return False
+
+_COMBINING = (
+        (0x300, 0x36f), (0x483, 0x489), (0x591, 0x5bd),
+        (0x5bf, 0x5bf), (0x5c1, 0x5c2), (0x5c4, 0x5c5),
+        (0x5c7, 0x5c7), (0x600, 0x603), (0x610, 0x61a),
+        (0x64b, 0x65e), (0x670, 0x670), (0x6d6, 0x6e4),
+        (0x6e7, 0x6e8), (0x6ea, 0x6ed), (0x70f, 0x70f),
+        (0x711, 0x711), (0x730, 0x74a), (0x7a6, 0x7b0),
+        (0x7eb, 0x7f3), (0x816, 0x819), (0x81b, 0x823),
+        (0x825, 0x827), (0x829, 0x82d), (0x901, 0x902),
+        (0x93c, 0x93c), (0x941, 0x948), (0x94d, 0x94d),
+        (0x951, 0x954), (0x962, 0x963), (0x981, 0x981),
+        (0x9bc, 0x9bc), (0x9c1, 0x9c4), (0x9cd, 0x9cd),
+        (0x9e2, 0x9e3), (0xa01, 0xa02), (0xa3c, 0xa3c),
+        (0xa41, 0xa42), (0xa47, 0xa48), (0xa4b, 0xa4d),
+        (0xa70, 0xa71), (0xa81, 0xa82), (0xabc, 0xabc),
+        (0xac1, 0xac5), (0xac7, 0xac8), (0xacd, 0xacd),
+        (0xae2, 0xae3), (0xb01, 0xb01), (0xb3c, 0xb3c),
+        (0xb3f, 0xb3f), (0xb41, 0xb43), (0xb4d, 0xb4d),
+        (0xb56, 0xb56), (0xb82, 0xb82), (0xbc0, 0xbc0),
+        (0xbcd, 0xbcd), (0xc3e, 0xc40), (0xc46, 0xc48),
+        (0xc4a, 0xc4d), (0xc55, 0xc56), (0xcbc, 0xcbc),
+        (0xcbf, 0xcbf), (0xcc6, 0xcc6), (0xccc, 0xccd),
+        (0xce2, 0xce3), (0xd41, 0xd43), (0xd4d, 0xd4d),
+        (0xdca, 0xdca), (0xdd2, 0xdd4), (0xdd6, 0xdd6),
+        (0xe31, 0xe31), (0xe34, 0xe3a), (0xe47, 0xe4e),
+        (0xeb1, 0xeb1), (0xeb4, 0xeb9), (0xebb, 0xebc),
+        (0xec8, 0xecd), (0xf18, 0xf19), (0xf35, 0xf35),
+        (0xf37, 0xf37), (0xf39, 0xf39), (0xf71, 0xf7e),
+        (0xf80, 0xf84), (0xf86, 0xf87), (0xf90, 0xf97),
+        (0xf99, 0xfbc), (0xfc6, 0xfc6), (0x102d, 0x1030),
+        (0x1032, 0x1032), (0x1036, 0x1037), (0x1039, 0x103a),
+        (0x1058, 0x1059), (0x108d, 0x108d), (0x1160, 0x11ff),
+        (0x135f, 0x135f), (0x1712, 0x1714), (0x1732, 0x1734),
+        (0x1752, 0x1753), (0x1772, 0x1773), (0x17b4, 0x17b5),
+        (0x17b7, 0x17bd), (0x17c6, 0x17c6), (0x17c9, 0x17d3),
+        (0x17dd, 0x17dd), (0x180b, 0x180d), (0x18a9, 0x18a9),
+        (0x1920, 0x1922), (0x1927, 0x1928), (0x1932, 0x1932),
+        (0x1939, 0x193b), (0x1a17, 0x1a18), (0x1a60, 0x1a60),
+        (0x1a75, 0x1a7c), (0x1a7f, 0x1a7f), (0x1b00, 0x1b03),
+        (0x1b34, 0x1b34), (0x1b36, 0x1b3a), (0x1b3c, 0x1b3c),
+        (0x1b42, 0x1b42), (0x1b44, 0x1b44), (0x1b6b, 0x1b73),
+        (0x1baa, 0x1baa), (0x1c37, 0x1c37), (0x1cd0, 0x1cd2),
+        (0x1cd4, 0x1ce0), (0x1ce2, 0x1ce8), (0x1ced, 0x1ced),
+        (0x1dc0, 0x1de6), (0x1dfd, 0x1dff), (0x200b, 0x200f),
+        (0x202a, 0x202e), (0x2060, 0x2063), (0x206a, 0x206f),
+        (0x20d0, 0x20f0), (0x2cef, 0x2cf1), (0x2de0, 0x2dff),
+        (0x302a, 0x302f), (0x3099, 0x309a), (0xa66f, 0xa66f),
+        (0xa67c, 0xa67d), (0xa6f0, 0xa6f1), (0xa806, 0xa806),
+        (0xa80b, 0xa80b), (0xa825, 0xa826), (0xa8c4, 0xa8c4),
+        (0xa8e0, 0xa8f1), (0xa92b, 0xa92d), (0xa953, 0xa953),
+        (0xa9b3, 0xa9b3), (0xa9c0, 0xa9c0), (0xaab0, 0xaab0),
+        (0xaab2, 0xaab4), (0xaab7, 0xaab8), (0xaabe, 0xaabf),
+        (0xaac1, 0xaac1), (0xabed, 0xabed), (0xfb1e, 0xfb1e),
+        (0xfe00, 0xfe0f), (0xfe20, 0xfe26), (0xfeff, 0xfeff),
+        (0xfff9, 0xfffb), (0x101fd, 0x101fd), (0x10a01, 0x10a03),
+        (0x10a05, 0x10a06), (0x10a0c, 0x10a0f), (0x10a38, 0x10a3a),
+        (0x10a3f, 0x10a3f), (0x110b9, 0x110ba), (0x1d165, 0x1d169),
+        (0x1d16d, 0x1d182), (0x1d185, 0x1d18b), (0x1d1aa, 0x1d1ad),
+        (0x1d242, 0x1d244), (0xe0001, 0xe0001), (0xe0020, 0xe007f),
+        (0xe0100, 0xe01ef), )
+
+def _ucp_width(ucs, control_chars='guess'):
+    # test for 8-bit control characters
+
+    # Don't understand why but this is needed for Python 3
+    ucs = ucs[0]
+
+    if (ucs < 32) or ((ucs < 0xa0) and (ucs >= 0x7f)):
+        # Control character detected
+        if control_chars == 'strict':
+            raise ControlCharError(b_('_ucp_width does not understand how to'
+                ' assign a width value to control characters.'))
+        if ucs in (0x08, 0x07F, 0x94):
+            # Backspace, delete, and clear delete remove a single character
+            return -1
+        if ucs == 0x1b:
+            # Excape is tricky.  It removes some number of characters that
+            # come after it but the amount is dependent on what is
+            # interpreting the code.
+            # So this is going to often be wrong but other values will be
+            # wrong as well.
+            return -1
+        # All other control characters get 0 width
+        return 0
+
+    if _interval_bisearch(ucs, _COMBINING):
+        # Combining characters return 0 width as they will be combined with
+        # the width from other characters
+        return 0
+
+    # if we arrive here, ucs is not a combining or C0/C1 control character
+
+    return (1 +
+      (ucs >= 0x1100 and
+       (ucs <= 0x115f or                     # Hangul Jamo init. consonants
+        ucs == 0x2329 or ucs == 0x232a or
+        (ucs >= 0x2e80 and ucs <= 0xa4cf and
+         ucs != 0x303f) or                   # CJK ... Yi
+        (ucs >= 0xac00 and ucs <= 0xd7a3) or # Hangul Syllables
+        (ucs >= 0xf900 and ucs <= 0xfaff) or # CJK Compatibility Ideographs
+        (ucs >= 0xfe10 and ucs <= 0xfe19) or # Vertical forms
+        (ucs >= 0xfe30 and ucs <= 0xfe6f) or # CJK Compatibility Forms
+        (ucs >= 0xff00 and ucs <= 0xff60) or # Fullwidth Forms
+        (ucs >= 0xffe0 and ucs <= 0xffe6) or
+        (ucs >= 0x20000 and ucs <= 0x2fffd) or
+        (ucs >= 0x30000 and ucs <= 0x3fffd))))
+
+def textual_width(msg, control_chars='guess', encoding='utf-8',
+        errors='replace'):
+
+    return sum(
+            # calculate width of each char
+            itermap(_ucp_width,
+                # Setup the arguments to _ucp_width
+                iterzip(
+                    # int value of each char
+                    itermap(ord, msg),
+                    # control_chars arg in a form that izip will deal with
+                    itertools.repeat(control_chars))))
