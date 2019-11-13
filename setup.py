4d6084680a407305009b0d78029bb3361ff2d02d
Hugo <hugovk@users.noreply.github.com>
2019-11-13 18:03:02 +0200

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -9,14 +9,16 @@ with open('README.md') as f:
 setup(
     name='prettytable',
     version=version,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.* !=3.4.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: BSD License',
         'Topic :: Text Processing'
     ],
@@ -26,7 +28,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Luke Maurits',
     author_email='luke@maurits.id.au',
-    maintainer='Jazz Band',
+    maintainer='Jazzband',
     url='https://github.com/jazzband/prettytable',
     py_modules=['prettytable'],
     test_suite="prettytable_test",
