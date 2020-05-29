9d5b223cb3e4d13df1c5f7a2ca0d364246800260
Mark Fickett <mark.fickett@gmail.com>
2020-05-29 11:04:36 -0400

    Add get_csv_string.

--- prettytable.py
+++ prettytable.py
@@ -53,12 +53,14 @@ if py3k:
     uni_chr = chr
     from html.parser import HTMLParser
     from html import escape
+    from io import StringIO
 else:
     itermap = itertools.imap
     iterzip = itertools.izip
     uni_chr = unichr  # noqa: F821
     from HTMLParser import HTMLParser
     from cgi import escape
+    import StringIO
 
 # hrule styles
 FRAME = 0
@@ -1511,6 +1513,33 @@ class PrettyTable(object):
                 break
             kwargs["start"] += page_length
         return "\f".join(pages)
+
+    ##############################
+    # CSV STRING METHODS         #
+    ##############################
+    def get_csv_string(self, **kwargs):
+
+        """Return string representation of CSV formatted table in the current state
+
+        Keyword arguments are first interpreted as table formatting options, and
+        then any unused keyword arguments are passed to csv.writer(). For
+        example, get_csv_string(header=False, delimiter='\t') would use
+        header as a PrettyTable formatting option (skip the header row) and
+        delimiter as a csv.writer keyword argument.
+        """
+
+        options = self._get_options(kwargs)
+        csv_options = {
+            key: value for key, value in kwargs.items() if key not in options}
+        csv_buffer = StringIO()
+        csv_writer = csv.writer(csv_buffer, **csv_options)
+
+        if options.get('header'):
+            csv_writer.writerow(self._field_names)
+        for row in self._get_rows(options):
+            csv_writer.writerow(row)
+
+        return csv_buffer.getvalue()
 
     ##############################
     # JSON STRING METHODS        #
