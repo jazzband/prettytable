075bb807bf6e2226de07048ce2534614ecadee54
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-10-04 16:37:14 +0300

    Uncomment passing test case and add asserts

--- prettytable_test.py
+++ prettytable_test.py
@@ -129,29 +129,33 @@ class DeleteColumnTest(unittest.TestCase):
             table.del_column("City not-a-name")
 
 
-# class FieldnamelessTableTest(unittest.TestCase):
-#
-#    """Make sure that building and stringing a table with no fieldnames works fine"""
-#
-#    def setUp(self):
-#        self.x = PrettyTable()
-#        self.x.add_row(["Adelaide",1295, 1158259, 600.5])
-#        self.x.add_row(["Brisbane",5905, 1857594, 1146.4])
-#        self.x.add_row(["Darwin", 112, 120900, 1714.7])
-#        self.x.add_row(["Hobart", 1357, 205556, 619.5])
-#        self.x.add_row(["Sydney", 2058, 4336374, 1214.8])
-#        self.x.add_row(["Melbourne", 1566, 3806092, 646.9])
-#        self.x.add_row(["Perth", 5386, 1554769, 869.4])
-#
-#    def testCanStringASCII(self):
-#        self.x.get_string()
-#
-#    def testCanStringHTML(self):
-#        self.x.get_html_string()
-#
-#    def testAddFieldnamesLater(self):
-#        self.x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
-#        self.x.get_string()
+class FieldnamelessTableTest(unittest.TestCase):
+
+    """Make sure that building and stringing a table with no fieldnames works fine"""
+
+    def setUp(self):
+        self.x = PrettyTable()
+        self.x.add_row(["Adelaide", 1295, 1158259, 600.5])
+        self.x.add_row(["Brisbane", 5905, 1857594, 1146.4])
+        self.x.add_row(["Darwin", 112, 120900, 1714.7])
+        self.x.add_row(["Hobart", 1357, 205556, 619.5])
+        self.x.add_row(["Sydney", 2058, 4336374, 1214.8])
+        self.x.add_row(["Melbourne", 1566, 3806092, 646.9])
+        self.x.add_row(["Perth", 5386, 1554769, 869.4])
+
+    def testCanStringASCII(self):
+        output = self.x.get_string()
+        assert "|  Field 1  | Field 2 | Field 3 | Field 4 |" in output
+        assert "|  Adelaide |   1295  | 1158259 |  600.5  |" in output
+
+    def testCanStringHTML(self):
+        output = self.x.get_html_string()
+        assert "<th>Field 1</th>" in output
+        assert "<td>Adelaide</td>" in output
+
+    def testAddFieldNamesLater(self):
+        self.x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+        assert "City name | Area | Population | Annual Rainfall" in self.x.get_string()
 
 
 class CityDataTest(unittest.TestCase):
@@ -854,7 +858,3 @@ class PrintEmojiTest(unittest.TestCase):
     def testPrint(self):
         print()
         print(self.x)
-
-
-if __name__ == "__main__":
-    unittest.main()
