c24df24734cb624cb2f2d5418b9ac63bd73e3134
Eric Tzimas <errikos.tzimas@gmail.com>
2021-05-05 13:50:45 +0300

    Add testing for add_autoindex function as per #90

--- tests/test_prettytable.py
+++ tests/test_prettytable.py
@@ -6,6 +6,7 @@ import sqlite3
 import unittest
 from math import e, pi, sqrt
 
+
 import pytest
 
 from prettytable import (
@@ -968,6 +969,36 @@ def test_add_rows():
 
     # Assert
     assert str(t1) == str(t2)
+
+
+def test_autoindex():
+
+    """Testing that a table with a custom index row is
+    equal to the one produced by the function
+    .add_autoindex()
+    """
+    table1 = PrettyTable()
+    table1.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+    table1.add_row(["Adelaide", 1295, 1158259, 600.5])
+    table1.add_row(["Brisbane", 5905, 1857594, 1146.4])
+    table1.add_row(["Darwin", 112, 120900, 1714.7])
+    table1.add_row(["Hobart", 1357, 205556, 619.5])
+    table1.add_row(["Sydney", 2058, 4336374, 1214.8])
+    table1.add_row(["Melbourne", 1566, 3806092, 646.9])
+    table1.add_row(["Perth", 5386, 1554769, 869.4])
+    table1.add_autoindex()
+
+    table2 = PrettyTable()
+    table2.field_names = ["City name", "Area", "Population", "Annual Rainfall", "Index"]
+    table2.add_row(["Adelaide", 1295, 1158259, 600.5, 1])
+    table2.add_row(["Brisbane", 5905, 1857594, 1146.4, 2])
+    table2.add_row(["Darwin", 112, 120900, 1714.7, 3])
+    table2.add_row(["Hobart", 1357, 205556, 619.5, 4])
+    table2.add_row(["Sydney", 2058, 4336374, 1214.8, 5])
+    table2.add_row(["Melbourne", 1566, 3806092, 646.9, 6])
+    table2.add_row(["Perth", 5386, 1554769, 869.4, 7])
+
+    assert str(table1) == str(table2)
 
 
 class UnpaddedTableTest(unittest.TestCase):
